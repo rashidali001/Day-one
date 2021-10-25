05e4f20b03872e9308d2fd496d5c20f0999db3b1
rashidali001 <69434556+rashidali001@users.noreply.github.com>
2021-10-25 16:27:44 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function close1(){
+    var overlay=document.getElementById("overlay");
+    overlay.style.display="none";
+}
+function open1(){
+    var overlay=document.getElementById("overlay");
+    overlay.style.display="block";
+}
